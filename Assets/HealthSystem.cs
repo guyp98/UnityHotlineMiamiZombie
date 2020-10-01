75c9901a41f2e86a5957d2946bd532aa6f5bf963
guyp98 <guyporat98@gmail.com>
2020-10-01 19:20:02 +0300

    aaaa

--- Assets/HealthSystem.cs
+++ Assets/HealthSystem.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class HealthSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int health;
+    public int MinLimit;
+    public int MaxLimit;
 
-    // Update is called once per frame
-    void Update()
+    private int LimitCorrecteur(int current)
     {
-        
+        int correctTo= current;
+        if (current < MinLimit )
+        {
+            correctTo = MinLimit;
+        }
+        else if (current > MaxLimit)
+        {
+            correctTo = MaxLimit;
+        }
+
+        return correctTo;
+    }
+    public void addHealth(int amount)
+    {
+        health += amount;
+        health = LimitCorrecteur(health);
+    }
+    public void SubHealth(int amount)
+    {
+        health -= amount;
+        health = LimitCorrecteur(health);
+    }
+    private void ChangePlayerSpeed(int ChangeTo)
+    {
+        GetComponent<playerMovment>().moveSpeed = ChangeTo;
     }
 }
+
